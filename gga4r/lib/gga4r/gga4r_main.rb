5ebb1eac3dc1934cedf89781f4907e2083156130
Ben Prew <ben.prew@gmail.com>
2013-02-04 07:59:42 -0800

    remove generations, use single population

--- gga4r/lib/gga4r/gga4r_main.rb
+++ gga4r/lib/gga4r/gga4r_main.rb
@@ -1,54 +1,28 @@
 require "yaml"
 require "rubygems"
+require 'logger'
 
 class GeneticAlgorithm
-  attr_reader :generations, :p_combination, :p_mutation
-  
+
   # Must be initialized with a Array of chromosomes
   # To be a chomosome the object must implement the next methods:
   #  - fitness
   #  - recombine
   #  - mutate
   #  Accepts the next properties:
-  #   - extra_generations: adds given array of generations to the GeneticAlgorithm's own array of generations.
-  #   - p_combination: probability of combiantion ( by default 0.2 )
-  #   - p_mutation: probability of mutation ( by default 0.01 )
   #   - max_population: maximum number of individuals that are allowed to form a generation.
   #   - logger: logger to write messages if given.
-  def initialize(in_pop, prop = {})    
-    @generations = [in_pop]
-    @generations += prop[:extra_generations] if prop[:extra_generations]
-    @p_combination = prop[:p_combination] || 0.2
-    @p_mutation = prop[:p_mutation] || 0.01
+  def initialize(in_pop, prop = {})
     @max_population =  prop[:max_population]
-    @logger = prop[:logger] if prop[:logger]
-    @use_threads = prop[:use_threads] if prop[:use_threads]
-#    mean_fitness
-  end
-
-
-# HELPER METHODS
-
-  # Returns the number of generations that are in the GeneticAlgorithm object.
-  def num_generations
-    @generations.size - 1
+    @logger = prop[:logger] || Logger.new('/dev/null')
+    @population = in_pop
+    @generations = []
   end
 
   # Returns an array with the best fitted individuals for given
   # generation number ( by default from last generation ).
-  def best_fit(num_generation = -1)
-    raise "Generation not generated still num generations = #{num_generations}" if num_generation > num_generations
-    @generations[num_generation].max { |a,b| a.fitness <=> b.fitness }
-  end
-  
-  # Returns the mean of the fitness for given
-  # generation number ( by default from last generation ).
-  def mean_fitness(num = -1)
-    raise "Generation not generated still num generations = #{num_generations}" if num > self.num_generations
-    num = self.num_generations if num == -1
-    sum_fitness = 0
-    @generations[num].each { |chromosome| sum_fitness += chromosome.fitness }
-    sum_fitness.to_f / @generations[num].size.to_f
+  def best_fit
+    @population.max_by(&:fitness)
   end
 
   # Returns a GeneticAlgorithm object with the generations
@@ -56,7 +30,7 @@ class GeneticAlgorithm
   # Files must contain the chromosomes in YAML format.
   def self.populate_from_files(a_filenames, prop = {})
     a_filenames = [a_filenames] if a_filenames.class == String
-    
+
     loaded_generations = a_filenames.collect { |filename| YAML.load(File.open(filename, "r")) }
     prop[:extra_generations] = loaded_generations[1..-1] if loaded_generations.size > 1
     return GeneticAlgorithm.new(loaded_generations[0], prop)
@@ -67,68 +41,26 @@ class GeneticAlgorithm
   def save_generation(filename, num_generation = -1)
     f = File.new(filename, "w")
     f.write(self.generations[num_generation].to_yaml)
-    f.close    
+    f.close
   end
 
 # EVOLUTION METHODS
 
   # Evolves the actual generation num_steps steps (1 by default).
-  def evolve(num_steps = 1)
-    num_steps.times do
-      @generations << evaluation_with_threads(@generations[-1])
-      selection!
-      recombination! 
-      mutation!
-    end
-  end
-  
-  # Prepares given generation for evaluation ( evaluates its fitness ).
-  def evaluation(g)
-    @logger.debug "Evaluation " + g.size.to_s + " chromosomes." if @logger
-    i = 0
-    g.collect do |chromosome|
-      i += 1
-      @logger.debug "Evaluating chromosome #{i}:" if @logger
-      @logger.debug "#{chromosome.stats.join("\n")}" if @logger
-      chromosome.fitness
-      chromosome
-    end
+  def evolve
+    @population = selection(@population)
+    new_gen = @population.map { |chromosome| chromosome.dup }
+    @population += recombination(new_gen) + mutation(new_gen)
   end
 
-  # Prepares given generation for evaluation ( evaluates its fitness ),
-  # using Threads
-  def evaluation_with_threads(g)
-    @logger.debug "Evaluation " + g.size.to_s + " chromosomes." if @logger
-    threads = []
-    i = 0
-    g.each do |chromosome|
-      i += 1
-      @logger.debug "Evaluating chromosome #{i}:" if @logger
-      @logger.debug "#{chromosome.stats.join("\n")}" if @logger
-      threads << Thread.new(chromosome) do |t_chromosome|
-        t_chromosome.fitness
-        @logger.debug "Thread finished #{Thread.current.object_id} - #{Thread.current.status}" if @logger
-      end
-    end
-    # Wait for threads for finish
-		threads.each do |thread|
-			@logger.debug "#{thread.status}" if @logger
-			thread.join
-			@logger.debug "#{thread.status}" if @logger
-		end
-    return g
-  end
+  private
 
-  
   # Selects population to survive and recombine
   def selection(g)
-    g_tmp = remainder_stochastic_sampling(g)
-    g_tmp = g_tmp.sort_by {|i| -i.fitness }[0..(@max_population-1)] if @max_population && (g_tmp.size > @max_population)
-    g_tmp
+    @max_population && g.length > @max_population ? g.sort {|a, b| b.fitness <=> a.fitness }[0..(@max_population-1)] : g
   end
-  def selection!; @generations[-1] = selection(@generations[-1]); end
 
-  # Recombines population  
+  # Recombines population
   def recombination(g)
     @logger.debug "Recombination " + g.size.to_s + " chromosomes." if @logger
     new_generation = g.dup.shuffle!
@@ -136,41 +68,20 @@ class GeneticAlgorithm
     new_children = []
     new_generation.each_slice(2) do |chromosome1, chromosome2|
       next if chromosome2.nil?
-      if rand > (1 - @p_combination)
-        @logger.debug "Recombining" if @logger
-        new_children << chromosome1.recombine(chromosome2)
-      end
+      @logger.debug "Recombining" if @logger
+      new_children << chromosome1.recombine(chromosome2)
     end
-    new_generation + new_children    
+    new_generation + new_children
   end
-
-  def recombination!; @generations[-1] = recombination(@generations[-1]); end
 
   # Mutates population
   def mutation(g)
-    @logger.debug "Mutation " + g.size.to_s + " chromosomes." if @logger  
+    @logger.debug "Mutation " + g.size.to_s + " chromosomes." if @logger
     new_generation = g.dup
     new_generation.each do |chromosome|
-      if rand > (1 - @p_mutation)
-        @logger.debug "Mutate" if @logger
-        chromosome.mutate 
-      end
-    end
-  end
-  def mutation!; @generations[-1] = mutation(@generations[-1]); end
-  
-  # Remainder Stochastic Sampling algorithm for selection.
-  def remainder_stochastic_sampling(g)
-    new_generation = []
-    g.each do |chromosome|
-      num_rep = 0
-      if chromosome.fitness > 0
-        num_rep += (chromosome.fitness.to_f/mean_fitness).to_i
-        num_rep += 1 if rand > (1 - (chromosome.fitness/mean_fitness)%1)
-      end
-      new_generation = new_generation + ([chromosome] * num_rep)
+      @logger.debug "Mutate" if @logger
+      chromosome.mutate
     end
     new_generation
   end
-
 end
--- gga4r/test/gga4r_test.rb
+++ gga4r/test/gga4r_test.rb
@@ -1,9 +1,17 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
 class IndividualStub < Array
+
+  def mutate
+  end
+
+  def fitness
+  end
+
   def recombine(a)
     self * 2
   end
+
   def self.create_random_population(num_population = 30)
     population = []
     num_population.times do
@@ -14,24 +22,12 @@ class IndividualStub < Array
 end
 
 class Gga4rTest < Test::Unit::TestCase
-
-  def setup
-  end
-  
-  def test_truth
-    assert true
-  end
-  
-  def test_recombine
+  def test_evolve
     first_pop_even = IndividualStub.create_random_population(30)
     ga = GeneticAlgorithm.new(first_pop_even)
-    new_pop = ga.recombination!
-    assert true, first_pop_even.size < new_pop.size
-    
-    first_pop_odd = IndividualStub.create_random_population(31)
-    ga = GeneticAlgorithm.new(first_pop_odd)
-    new_pop = ga.recombination!
-    assert true, first_pop_odd.size < new_pop.size
-    
+    ga.evolve()
+    new_pop = ga.instance_variable_get(:@population)
+
+    assert first_pop_even.size < new_pop.size
   end
 end
