a29348c11bc43ae9266b3e5ef4758391a0887592
Pablo Carranza Vélez <pablocarranza@gmail.com>
2013-10-05 21:32:44 -0300

    Add deterministic crowding for multi-modal optimization

--- lib/gga4r.rb
+++ lib/gga4r.rb
@@ -1,6 +1,6 @@
 require 'logger'
 
-# Modification of gga4r to allow for multi-modal optimization
+# Modification of gga4r to allow for multi-modal optimization with deterministic crowding
 class GeneticAlgorithm
   VERSION = '0.9.3'
 
@@ -9,25 +9,28 @@ class GeneticAlgorithm
   #  - fitness
   #  - recombine
   #  - mutate
+  #  - distance (only for multi-modal optimization)
   #  Accepts the next properties:
   #   - max_population: maximum number of individuals that are allowed to form a generation.
   #   - logger: logger to write messages if given.
+  #
+  # This GA will always mutate and recombine. Probabilities should be defined and evaluated in the chromosome class.
   def initialize(in_pop, prop = {})
     @max_population =  prop[:max_population]
     @logger = prop[:logger] || Logger.new('/dev/null')
     @population = in_pop
     @generations = []
+    @multi_modal = prop[:multi_modal]
   end
 
-  # Returns an array with the best fitted individuals for given
-  # generation number ( by default from last generation ).
+  # Returns an array with the best fitted individuals for last generation
   def best_fit
     @population.max_by(&:fitness)
   end
 
   # Returns an array with the best fitted n individuals from the population (might include local optima)
   def best_fitted n
-    @population.sort_by{|c| -c.fitness}.first(n)
+    @population.uniq.sort_by{|c| -c.fitness}.first(n)
   end
 
   # Returns a GeneticAlgorithm object with the generations
@@ -48,17 +51,24 @@ class GeneticAlgorithm
 # EVOLUTION METHODS
 
   # Evolves the actual generation num_steps steps (1 by default).
-  def evolve
-    @population = selection(@population)
-    new_gen = @population.map { |chromosome| chromosome.dup }
-    @population += recombination(new_gen) + mutation(new_gen)
+  def evolve num_steps = 1
+    num_steps.times do |t|
+      @population = selection(@population)
+      if !@multi_modal
+        new_gen = @population.map { |chromosome| chromosome.dup }
+        @population += recombination(new_gen) + mutation(new_gen)
+      else
+        @population = deterministic_crowding(@population)
+        @population = mutation(@population)
+      end
+    end
   end
 
   private
 
   # Selects population to survive and recombine
   def selection(g)
-    @max_population && g.length > @max_population ? g.sort_by{|c| -c.fitness}.first(@max_population) : g
+      @max_population && g.length > @max_population ? g.sort_by{|c| -c.fitness}.first(@max_population) : g
   end
 
   # Recombines population
@@ -73,6 +83,29 @@ class GeneticAlgorithm
       new_children << chromosome1.recombine(chromosome2)
     end
     new_generation + new_children
+  end
+
+  def deterministic_crowding(g)
+    group1 = g.sample(g.length/2)
+    group2 = (g - group1).shuffle
+
+    new_gen = []
+
+    group1.each_with_index do |p1,i|
+      p2 = group2[i]
+
+      c1, c2 = p1.recombine(p2)
+
+      if p1.distance(c1) + p2.distance(c2) <= p1.distance(c2) + p2.distance(c1)
+        new_gen << [p1,c1].max_by(&:fitness)
+        new_gen << [p2,c2].max_by(&:fitness)
+      else 
+        new_gen << [p1,c2].max_by(&:fitness)
+        new_gen << [p2,c1].max_by(&:fitness)
+      end
+
+    end
+    new_gen
   end
 
   # Mutates population
