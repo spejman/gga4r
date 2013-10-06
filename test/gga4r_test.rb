a095eee9a9b02240711264917d081888e4cda8b8
Hank Stoever <hstove@gmail.com>
2013-10-05 18:15:46 -0700

    Adds test case to demonstrate failing example

--- test/gga4r_test.rb
+++ test/gga4r_test.rb
@@ -21,13 +21,48 @@ class IndividualStub < Array
   end
 end
 
+class StringPopulation < Array
+  def fitness
+    fitness = self.select { |pos| pos == 1 }.size.to_f / self.size.to_f
+    fitness
+  end
+
+  def recombine(c2)
+    cross_point = rand([self.size, c2.size].min) + 1
+    c1_a, c1_b = self.slice(0,cross_point), self.slice(cross_point, 0)
+    c2_a, c2_b = c2.slice(0,cross_point), c2.slice(cross_point, 0)
+    [StringPopulation.new(c1_a + c2_b), StringPopulation.new(c2_a + c1_b)]
+  end
+
+  def mutate
+    mutate_point = (rand * self.size).to_i
+    self[mutate_point] = 1
+  end
+
+  def self.create_population(s_long = 10, num = 10)
+    population = []
+    num.times  do
+      chromosome = self.new(Array.new(s_long).collect { (rand > 0.2) ? 0:1 })
+      population << chromosome
+    end
+    population
+  end
+end
+
 class Gga4rTest < Test::Unit::TestCase
-  def test_evolve
+  def test_single_recombination_result
     first_pop_even = IndividualStub.create_random_population(30)
     ga = GeneticAlgorithm.new(first_pop_even)
-    ga.evolve()
-    new_pop = ga.instance_variable_get(:@population)
+    5.times do |i|
+      ga.evolve
+      new_pop = ga.instance_variable_get(:@population)
+      assert first_pop_even.size < new_pop.size
+    end
+  end
 
-    assert first_pop_even.size < new_pop.size
+  def test_multiple_recombination
+    ga = GeneticAlgorithm.new(StringPopulation.create_population)
+    4.times { |i| ga.evolve }
+    p ga.best_fit[0]
   end
 end
